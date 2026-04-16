747a7d5f269c2a5e4e1ed58f4de35e4339ac2997
Leon <leonlsqi@gmail.com>
2026-04-16 21:53:31 +0200

    structure

--- model.py
+++ model.py
@@ -7,11 +7,33 @@ class MiniAlexNet(nn.Module):
         super().__init__()
 
         self.features = nn.Sequential(
-
             # Conv1: RGB 32 * 32
             # 64 Channels Output
             nn.Conv2d(in_channels=3, out_channels=64,
-                      kernel_size=3, stride=1, padding=1)
+                      kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            # 32 * 32 -> 16 * 16
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
+            # Conv2
+            nn.Conv2d(in_channels=64, out_channels=192,
+                      kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            # 16x16 -> 8x8
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
+            # Conv3
+            nn.Conv2d(192, 384, 3, 1),
+            nn.ReLU(True),
+
+            # Con4
+            nn.Conv2d(384, 256, 3, 1),
+            nn.ReLU(True),
+
+            # Conv5
+            nn.Conv2d(256, 256, 3, 1),
+            nn.ReLU(True),
+
+            # 8*8 -> 4*4
+            nn.MaxPool2d(kernel_size=2, stride=2),
         )
