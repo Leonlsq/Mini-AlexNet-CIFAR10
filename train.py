ad9cda7491ffc26b34324052ee5e3e5d346ddca4
Leon <leonlsqi@gmail.com>
2026-04-17 20:26:36 +0200

    training

--- train.py
+++ train.py
@@ -17,3 +17,40 @@ criterion = nn.CrossEntropyLoss()
 
 # emprically I choose 0.001 as Learning Rate
 optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+num_epochs = 5
+
+print("Start training...")
+
+for epoch in range(num_epochs):
+    model.train()
+    tot_loss = 0
+
+    for i, (images, labels) in enumerate(train_loader):
+
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # last gradients to 0
+        optimizer.zero_grad()
+
+        # forward
+        outputs = model(images)
+
+        cur_loss = criterion(outputs, labels)
+        cur_loss.backward()
+
+        # perform optimization step with backpropagated gradients
+        optimizer.step()
+
+        # accumulate total loss
+        tot_loss += cur_loss.item()
+
+        # print the average loss every 100 batches
+        if (i+1) % 100 == 0:
+            print(
+                f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader)}], Loss: {running_loss/100:.4f}')
+            running_loss = 0.0
+
+
+print("Training complete")
